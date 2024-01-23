5efd291ade6774f375ea96113517ef3cf6564813
AidonDaehling <157511139+AidonDaehling@users.noreply.github.com>
2024-01-23 10:25:10 -0600

    Add files via upload

---
+++ Lab2c.cpp
@@ -0,0 +1,12 @@
+#include<iostream> //this "header file" allows the use of cout
+					//(put characters OUT - to the terminal)
+
+using namespace std; //standard
+
+int main()
+{
+	//new "paragraph
+	cout << "Hello World" << endl
+
+}
+
